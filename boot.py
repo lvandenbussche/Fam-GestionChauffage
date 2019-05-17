280d73e1e5303b9659b1416961ddf6c928cd2db3
lvandenbussche <lionel.vandenbussche@gmail.com>
2019-05-17 16:02:24 +0200

    Add webrepl server for debug

--- boot.py
+++ boot.py
@@ -1,9 +1,12 @@
 import ConnectWiFi
+import webrepl
+import time
+
 
 W = ConnectWiFi.ConnectWifi('101', 'wifigest')
 W.connect_wlan()
 W.disable_wlan_ap()
+time.sleep(2)
 
-import webrepl
 webrepl.start()
 
