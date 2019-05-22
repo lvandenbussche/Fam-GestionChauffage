801c7853305b62719502e592b6b48085aac0f47a
lvandenbussche <lionel.vandenbussche@gmail.com>
2019-05-22 13:27:45 +0200

    Origine

--- MyNode.py
+++ MyNode.py
@@ -1,8 +1,8 @@
 from mqtt_as import MQTTClient, config
 import uasyncio as asyncio
-from main.KMPDinoESP import KMPDinoESP
+from KMPDinoESP import KMPDinoESP
 from utime import ticks_ms, ticks_diff
-from main import tools
+import tools
 import ubinascii
 import dht as dht22
 import machine
--- boot.py
+++ boot.py
@@ -1,9 +1,9 @@
-from main import ConnectWiFi
+import ConnectWiFi
 import webrepl
 import time
 
 
-W = ConnectWiFi.ConnectWifi('101', 'wifigest')
+W = ConnectWiFi.ConnectWifi('Reseau Wi-Fi de Lionel', 'Klat0Verata')
 W.connect_wlan()
 W.disable_wlan_ap()
 time.sleep(2)
